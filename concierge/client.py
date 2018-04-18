361b8c487918331fecee01c9c2774c10f2666264
Nickolaus D. Saint <nick@globus.org>
2018-04-18 08:51:37 -0700

    Added support for BDBag metadata

--- concierge/client.py
+++ concierge/client.py
@@ -25,26 +25,26 @@ def globus():
 
 
 @main.command(help='Create a BDBag with a Remote File Manifest')
+@click.option('--metadata', '-m', type=click.File('r'), nargs=1)
 @click.option('--server', '-s', help='Concierge server to use',
               default='https://concierge.fair-research.org')
-@click.argument('remote_file_manifest')
+@click.argument('remote_file_manifest', type=click.File('r'))
 @click.argument('title')
-def create(remote_file_manifest, title, server):
-    if not os.path.exists(remote_file_manifest):
-        click.echo('Remote File Manifest "{}" '
-                   'does not exist'.format(remote_file_manifest))
-        sys.exit(2)
-    with open(remote_file_manifest) as f:
-        try:
-            # this should take an optional metadata
-            info = get_info()
-            access_token = info['tokens']['auth.globus.org']['access_token']
-            rfm_file = json.loads(f.read())
-            bag = create_bag(server, rfm_file, info['name'],
-                             info['email'], title, access_token)
-            click.echo('{}'.format(bag['minid_id']))
-        except ConciergeException as ce:
-            click.echo('Error Creating Bag: {}'.format(ce.message))
+def create(remote_file_manifest, title, server, metadata):
+    try:
+        # this should take an optional metadata
+        info = get_info()
+        access_token = info['tokens']['auth.globus.org']['access_token']
+        rfm_file = json.loads(remote_file_manifest.read())
+        bag_metadata = {}
+        if metadata:
+            bag_metadata = json.loads(metadata.read())
+        bag = create_bag(server, rfm_file, info['name'],
+                         info['email'], title, access_token,
+                         metadata=bag_metadata)
+        click.echo('{}'.format(bag['minid_id']))
+    except ConciergeException as ce:
+        click.echo('Error Creating Bag: {}'.format(ce.message))
 
 
 def update(path, minid):
