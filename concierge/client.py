8419fa2054603bac55d2153edf058550210ecbbf
Nickolaus D. Saint <nick@globus.org>
2018-05-01 13:47:11 -0700

    Added warnings, connection error handling

--- concierge/client.py
+++ concierge/client.py
@@ -2,6 +2,7 @@ import os
 import sys
 import click
 import json
+import requests
 from concierge.globus_login import login as glogin
 from concierge.globus_login import get_info
 from concierge.api import create_bag
@@ -42,13 +43,17 @@ def create(remote_file_manifest, title, server, metadata, ro_metadata):
             bag_metadata = json.loads(metadata.read())
         if ro_metadata:
             ro_bag_metadata = json.loads(ro_metadata.read())
+        if any([isinstance(v, dict) for v in bag_metadata.values()]):
+            click.echo('Warning: Metadata contains complex objects.', err=True)
         bag = create_bag(server, rfm_file, info['name'],
                          info['email'], title, access_token,
                          metadata=bag_metadata,
                          ro_metadata=ro_bag_metadata)
         click.echo('{}'.format(bag['minid_id']))
     except ConciergeException as ce:
-        click.echo('Error Creating Bag: {}'.format(ce.message))
+        click.echo('Error Creating Bag: {}'.format(ce.message), err=True)
+    except requests.exceptions.ConnectionError as ce:
+        click.echo(str(ce), err=True)
 
 
 def update(path, minid):
