f2554d0c8ed55ea214453f4856186bb6d2bca564
Nickolaus D. Saint <nick@globus.org>
2018-08-16 11:00:19 -0700

    Fixed token for staging, added more informative staging output

--- concierge/client.py
+++ concierge/client.py
@@ -3,12 +3,17 @@ import sys
 import click
 import json
 import requests
+from functools import reduce
+from urllib.parse import urlencode
 from concierge.globus_login import login as glogin
 from concierge.globus_login import get_info
 from concierge.api import create_bag, stage_bag
 from concierge.exc import ConciergeException
 
 from concierge import DEFAULT_CONCIERGE_SERVER, CONCIERGE_SCOPE_NAME
+
+GLOBUS_WEB_TASK = 'https://app.globus.org/activity/{}/overview'
+GLOBUS_WEB_TRANSFER = 'https://app.globus.org/file-manager?{}'
 
 
 @click.group()
@@ -77,12 +82,30 @@ def stage(minids, destination_endpoint, path, server):
     try:
         info = get_info()
         # this should take an optional metadata
-        auth_token = info['tokens']['auth.globus.org']['access_token']
-        transfer_token = \
-            info['tokens']['transfer.api.globus.org']['access_token']
-        result = stage_bag(minids, destination_endpoint, auth_token,
+        bearer_token = info['tokens'][CONCIERGE_SCOPE_NAME]['access_token']
+        result = stage_bag(minids, destination_endpoint, bearer_token,
                            prefix=path, server=server)
-        click.echo('{}'.format(result))
+        stats = {
+            'transferred': len(reduce(lambda x, y: len(x) + len(y),
+                               result['transfer_catalog'].values())),
+            'source_eps': len(result['transfer_catalog']),
+            'dest': GLOBUS_WEB_TRANSFER.format(
+                urlencode({'origin_id': result['destination_endpoint'],
+                           'origin_path': result['destination_path_prefix']})),
+            'catalog': result['url'],
+            'transfer_tasks': ','.join([GLOBUS_WEB_TASK.format(s) for s in
+                                        result['transfer_task_ids']]),
+            'transfer_errors': len(result['error_catalog'])
+        }
+        report = (
+            'Files Transferred: \t{transferred}\n'
+            'Source Endpoints: \t{source_eps}\n'
+            'Destination Path: \t{dest}\n'
+            'Transfer Catalog: \t{catalog}\n'
+            'Transfer Tasks: \t{transfer_tasks}\n'
+            'Transfer Errors: \t{transfer_errors}'.format(**stats)
+        )
+        click.echo('{}'.format(report))
     except ConciergeException as ce:
         click.echo('Error Creating Bag: {}'.format(ce.message), err=True)
     except requests.exceptions.ConnectionError as ce:
